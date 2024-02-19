400cc1cb38aa43fb06afe0b095a3b56746ef5e06
Alamin <mdalaminhossain573190@gmail.com>
2024-02-19 11:51:15 +0600

    addeded some functional part 02

--- js/script.js
+++ js/script.js
@@ -1,26 +1,51 @@
 const allSeat = document.getElementsByClassName('seat');
 let count = 0;
 let count2 =40;
+let tottalPrice = 0;
 for (const seat of allSeat){
     seat.addEventListener('click',function(e){
         count = count + 1;
         count2 = count2 - 1;
+        // calculate tottal price
+        tottalPrice = tottalPrice + 550;
+        if(count <= 4){
+            // hidden and disable and color  
         const seatName =e.target.innerText;
         e.target.style.backgroundColor ='green';
-        // const seatPrice =e.target.parentNode.parentNode.parentNode;
-        // console.log(seatPrice)
-    //     const seatPriceContainer = document.getElementById('seat-price-containar');
-    //    const li = document.createElement('li');
-    //   const p = document.createElement('p');
-    //   p.innerText=e.target.innerText;
-
-    //   li.appendChild(p);
+        e.target.style.cursor ="not allowed";
+        e.target.style.pointerEvents ="none";
         
-        // seatPriceContainer.appendChild();
-        setInnerText('seat-count',count)
-        setInnerText('tottal-seat',count2)
+    //   append child
+        const seatPriceContainer = document.getElementById('seat-price-containar');
+       const li = document.createElement('li');
+      const p = document.createElement('p');
+      const p2 =document.createElement('p');
+      const SeatPrice =document.createElement('p');
+      p.innerText=seatName;
+      p2.innerText="Economoy";
+      SeatPrice.innerText=("550");
+     const SeatPriceNum = parseInt(SeatPrice.innerText)
+    //   seatPrice =parseInt(p3.innerText);
+    // console.log(typeof SeatPriceNum);
+       
+      li.appendChild(p);
+      li.appendChild(p2); 
+      li.appendChild(SeatPrice);
+        
+        seatPriceContainer.appendChild(li);
+        setInnerText('tottal-price',tottalPrice);
+        setInnerText('seat-count',count);
+        setInnerText('tottal-seat',count2);
+        }
+        else {
+            seat.setAttribute('disable' ,true)
+        }
     })
 }
+
 function setInnerText(id, value){
     document.getElementById(id).innerText=value;
-}
+} 
+
+const btn =document.getElementById('copon-btn');
+console.log(btn)
